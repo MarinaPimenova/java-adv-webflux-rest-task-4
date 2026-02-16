ed57c5d68c44e629c30eb7cb49e5475eb0d22876
Pimenova, Marina <mnfpma@gmail.com>
2026-02-16 22:40:43 +0200

    first commit

---
+++ src/main/java/com/hw/rest/JavaAdvWebfluxRestTask4Application.java
@@ -0,0 +1,13 @@
+package com.hw.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvWebfluxRestTask4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdvWebfluxRestTask4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hw/rest/JavaAdvWebfluxRestTask4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdvWebfluxRestTask4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
